53d7b0b11491a061f2882fdbc58faa956e7f42d7
Varun Adit <varunadit2@gmail.com>
2024-01-09 10:11:31 -0500

    Initial Commit

---
+++ UIKitNetworkingPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitNetworkingPractice
+//
+//  Created by Varun Adit on 1/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
